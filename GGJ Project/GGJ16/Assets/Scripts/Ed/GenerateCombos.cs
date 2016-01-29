ac291667ffda8e9e4b87c742c35839cc0c3e73aa
Edward Whitehead <ewhitehead@aetheralchemist.com.au>
2016-01-30 10:05:00 +1100

    Basic combo system working, Keycode based

---
+++ GGJ Project/GGJ16/Assets/Scripts/Ed/GenerateCombos.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateCombos : MonoBehaviour
+{
+    //VARIABLES
+    //Combos
+    private List<KeyCode> P1Combo;
+    private List<KeyCode> P2Combo;
+    //Input list
+    public List<KeyCode> P1Inputs;
+    public List<KeyCode> P2Inputs;
+    //Number of elements in combos
+    private int p1KeyCount;
+    private int p2KeyCount;
+    //Difficulty level for players
+    private int p1Difficulty;
+    private int p2Difficulty;
+
+    void Awake()
+    {
+        p1KeyCount = 4;
+        p2KeyCount = 4;
+    }
+
+	// Use this for initialization
+	public List<KeyCode> Generate (int player)
+    {
+        if (player == 0)
+        {
+            //Player one combo generation
+            P1Combo = new List<KeyCode>();
+            for (int i = 0; i < p1KeyCount; i++)
+            {
+                P1Combo.Add(P1Inputs[Random.Range(0, p1KeyCount)]);
+            }
+            //Pass the combo to the player
+            return P1Combo;
+        }
+        else
+        {
+            //Player one combo generation
+            P2Combo = new List<KeyCode>();
+            for (int i = 0; i < p2KeyCount; i++)
+            {
+                P2Combo.Add(P2Inputs[Random.Range(0, p2KeyCount)]);
+            }
+            //Pass the combo to the player
+            return P2Combo;
+        }
+	}
+}
---
+++ GGJ Project/GGJ16/Assets/Scripts/Ed/SpiderCombo.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpiderCombo : MonoBehaviour
+{
+    public GenerateCombos gen;
+    public int player;
+    public List<KeyCode> combo;
+    private float timer;
+    private int curStep;
+
+    public int health;
+    private KeyCode keyPress;
+
+	// Use this for initialization
+	void Start ()
+    {
+        NewCombo();
+	}
+
+    void OnGUI()
+    {
+        //Has to be in OnGUI for some reason...
+        keyPress = Event.current.keyCode;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.smoothDeltaTime;
+        //Check there's a keypress
+        if (keyPress != KeyCode.None)
+        {
+            //make sure it's one of our keys
+            bool hasKey = player == 0 ? gen.P1Inputs.Contains(keyPress) : gen.P2Inputs.Contains(keyPress);
+            if (hasKey)
+            {
+                //step through the combo
+                if (curStep < combo.Count)
+                {
+                    //Debug.Log(timer);
+                    if (combo[curStep] == keyPress)
+                    {
+                        curStep++;
+                        health++;
+                        if(curStep == combo.Count)
+                        {
+                            NewCombo();
+                        }
+                    }
+                    else
+                    {
+                        health--;
+                    }
+                }
+            }
+        }
+	}
+
+    //get us a new combo, pass timer to help calculate score
+    void NewCombo()
+    {
+        combo = gen.Generate(player);
+        curStep = 0;
+        timer = 0;
+        //Debug.Log("Return");
+    }
+}
