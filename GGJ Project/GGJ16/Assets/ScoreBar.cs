e8ecf535fb136ede23550136f2665f60324013b1
Ronald Chau <ronaldchau8@hotmail.com>
2016-01-30 18:19:19 +1100

    Score Slider

---
+++ GGJ Project/GGJ16/Assets/ScoreBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreBar : MonoBehaviour 
+{
+	[SerializeField]
+	private ScoreManager _scoreManager;
+	private Slider _slider;
+
+	void Start()
+	{
+		_slider = gameObject.GetComponent<Slider>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		_slider.value = _scoreManager.GetScorePercentage();
+	}
+}
