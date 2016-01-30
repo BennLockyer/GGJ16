3551d755515c37f5cd067373a0c09d3acbfb815e
Benn Lockyer <benn.lockyer@hotmail.com>
2016-01-30 17:49:28 +1100

    Updates to female spider

---
+++ GGJ Project/GGJ16/Assets/Scripts/FemaleLookAt.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FemaleLookAt : MonoBehaviour
+{
+    public Vector3 player1pos;
+    public Vector3 player2pos;
+
+    private Vector3 startRotation;
+
+    private ScoreManager scoreManager;
+    void Awake()
+    {
+        player1pos = GameObject.FindWithTag("Player1").transform.position;
+        player2pos = GameObject.FindWithTag("Player2").transform.position;
+        scoreManager = GameObject.FindWithTag("GameManager").GetComponent<ScoreManager>();
+        startRotation = transform.eulerAngles;
+    }
+	
+    void Update ()
+    {
+	    if(scoreManager.currentScore == 0)
+        {
+            transform.eulerAngles = startRotation;
+        }
+        else if(scoreManager.currentScore > 0)
+        {
+            transform.LookAt(player1pos);
+            transform.eulerAngles = new Vector3(startRotation.x, transform.eulerAngles.y, startRotation.z);
+        }
+        else
+        {
+            transform.LookAt(player2pos);
+            transform.eulerAngles = new Vector3(startRotation.x, transform.eulerAngles.y, startRotation.z);
+        }
+	}
+}
---
+++ GGJ Project/GGJ16/Assets/Scripts/FemaleParticles.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FemaleParticles : MonoBehaviour
+{
+    public GameObject heartParticles;
+    public GameObject dinnerParticles;
+
+    public float particleTime;
+
+    private ScoreManager scoreManager;
+    public float timer;
+    void Awake()
+    {
+        scoreManager = GameObject.FindWithTag("GameManager").GetComponent<ScoreManager>();
+        heartParticles.SetActive(false);
+        dinnerParticles.SetActive(false);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= particleTime)
+        {
+            timer -= particleTime;
+            heartParticles.SetActive(false);
+            dinnerParticles.SetActive(false);
+
+            if (scoreManager.currentScore == 0) return;
+
+            int randomInt = Random.Range(0, 2);
+            float angle = 0;
+            if (randomInt == 0)
+            {
+                if (scoreManager.currentScore < 0)
+                    angle = -20f;
+                else
+                    angle = 20f;
+                heartParticles.transform.localEulerAngles = new Vector3(0, angle, 0);
+                heartParticles.SetActive(true);
+            }
+            else
+            {
+                if (scoreManager.currentScore > 0)
+                    angle = -20f;
+                else
+                    angle = 20f;
+                dinnerParticles.transform.localEulerAngles = new Vector3(0, angle, 0);
+                dinnerParticles.SetActive(true);
+            }
+            
+        }
+    }
+}
