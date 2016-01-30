cf44a12f66b1a43b7ed1baf09217543f23f44544
Ronald Chau <ronaldchau8@hotmail.com>
2016-01-30 12:42:01 +1100

    Hooked up Combo to Combo Display

---
+++ GGJ Project/GGJ16/Assets/Scripts/Keymap.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+// Intended for the purpose of Mapping keycodes to UI Images to be displayed
+
+public class Keymap : MonoBehaviour {
+	[SerializeField]
+	public Dictionary<KeyCode, string> _KeyMap;
+}
